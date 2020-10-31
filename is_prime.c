6b14d47666b5c171707811558ca03d01da5828ab
Yagnahaun Jonnadula <hanu5053nr@gmail.com>
2020-10-31 12:20:47 +0530

    Update is_prime

--- is_prime.c
+++ is_prime.c
@@ -14,11 +14,11 @@ int main(int argc, char **argv)
 
     if (isPrime)
     {
-        printf("%lld is a prime\n", num);
+        printf("%lld is \x1b[34ma prime\x1b[0m\n", num);
     }
     else
     {
-        printf("%lld is not a prime\n", num);
+        printf("%lld is \x1b[34mnot a prime\x1b[0m\n", num);
     }
 
     exit(0);
@@ -35,6 +35,7 @@ long long preProcess(int argc, char **argv)
     long long num = atol(argv[1]);
     if (num <= 1)
     {
+        printf("\x1b[31mInvalid Input\x1b[0m\n");
         printf("Please provide a positive number > 1\n");
         exit(2);
     }
